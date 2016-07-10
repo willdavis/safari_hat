aec1ceb3da77db0447e6c2f5ccfdbb06353e8ae7
wdavis <will.davis@hotschedules.com>
2016-07-10 16:06:33 -0700

    improved battery voltage monitoring using the bandgap

--- safari_hat.ino
+++ safari_hat.ino
@@ -4,12 +4,18 @@
 #define LED_PIN     5
 #define LED_PIN_2   6
 #define MIC_PIN     7 //A7
+
 #define COLOR_ORDER GRB
 #define CHIPSET     WS2811
 #define NUM_LEDS    40
 
 #define BRIGHTNESS  50
 #define FRAMES_PER_SECOND 120
+
+#define WARNING_VOLTAGE 350 //cV
+
+// Battery power states
+bool low_power_state = false;
 
 // microphone settings
 int micSensorValue = 0;
@@ -42,11 +48,6 @@ void loop()
   // reset brightness
   FastLED.setBrightness( BRIGHTNESS );
 
-  // read battery voltage
-  if (readVcc() <= 3000) {
-    EVERY_N_MILLISECONDS( 33 ){ digitalWrite(BUZZER_PIN, !digitalRead(BUZZER_PIN)); }
-  }
-
   // Read the current analog signal from the mic
   micSensorValue = analogRead(MIC_PIN);
 
@@ -58,6 +59,20 @@ void loop()
 
   EVERY_N_MILLISECONDS( 20 ) { current_hue++; } // slowly cycle the "base color" through the rainbow
   EVERY_N_SECONDS( 10 ) { next_pattern(); }     // change patterns periodically
+  EVERY_N_SECONDS( 1 ) { check_voltage(getBandgap()); }    // check if the battery voltage is below the limit
+
+  // beep if a low power state is detected
+  if (low_power_state == true) {
+    EVERY_N_MILLISECONDS( 50 ){ digitalWrite(BUZZER_PIN, !digitalRead(BUZZER_PIN)); }
+  }
+
+}
+
+void check_voltage(int voltage)
+{
+  // check for low power threshold
+  if (voltage <= WARNING_VOLTAGE && low_power_state == false ){ low_power_state = true; }
+  if (voltage > WARNING_VOLTAGE && low_power_state == true ){ low_power_state = false; digitalWrite(BUZZER_PIN, LOW); }
 }
 
 // add one to the current pattern number, and wrap around at the end
@@ -83,32 +98,27 @@ void confetti(int mic)
   leds_2[pos_2] += CHSV( current_hue + random8(64), 200, 255);
 }
 
-// Shamelessly stolen from:
-// http://provideyourown.com/2012/secret-arduino-voltmeter-measure-battery-voltage/
-long readVcc() {
-  // Read 1.1V reference against AVcc
-  // set the reference to Vcc and the measurement to the internal 1.1V reference
-  #if defined(__AVR_ATmega32U4__) || defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
-    ADMUX = _BV(REFS0) | _BV(MUX4) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
-  #elif defined (__AVR_ATtiny24__) || defined(__AVR_ATtiny44__) || defined(__AVR_ATtiny84__)
-    ADMUX = _BV(MUX5) | _BV(MUX0);
-  #elif defined (__AVR_ATtiny25__) || defined(__AVR_ATtiny45__) || defined(__AVR_ATtiny85__)
-    ADMUX = _BV(MUX3) | _BV(MUX2);
-  #else
-    ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
-  #endif  
-
-  delay(2); // Wait for Vref to settle
-  ADCSRA |= _BV(ADSC); // Start conversion
-  while (bit_is_set(ADCSRA,ADSC)); // measuring
-
-  uint8_t low  = ADCL; // must read ADCL first - it then locks ADCH  
-  uint8_t high = ADCH; // unlocks both
-
-  long result = (high<<8) | low;
-
-  result = 1125300L / result; // Calculate Vcc (in mV); 1125300 = 1.1*1023*1000
-  String msg = "Voltage: ";
-  Serial.println(msg + result);
-  return result; // Vcc in millivolts
+int getBandgap(void) // Returns actual value of Vcc (x 100)
+{
+#if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
+    // For mega boards
+    const long InternalReferenceVoltage = 1080L;  // Adjust this value to your boards specific internal BG voltage x1000
+    // REFS1 REFS0          --> 0 1, AVcc internal ref. -Selects AVcc reference
+    // MUX4 MUX3 MUX2 MUX1 MUX0  --> 11110 1.1V (VBG)         -Selects channel 30, bandgap voltage, to measure
+    ADMUX = (0<<REFS1) | (1<<REFS0) | (0<<ADLAR)| (0<<MUX5) | (1<<MUX4) | (1<<MUX3) | (1<<MUX2) | (1<<MUX1) | (0<<MUX0);
+#else
+    // For 168/328 boards
+    const long InternalReferenceVoltage = 1250L;  // Adjust this value to your boards specific internal BG voltage x1000
+    // REFS1 REFS0          --> 0 1, AVcc internal ref. -Selects AVcc external reference
+    // MUX3 MUX2 MUX1 MUX0  --> 1110 1.1V (VBG)         -Selects channel 14, bandgap voltage, to measure
+    ADMUX = (0<<REFS1) | (1<<REFS0) | (0<<ADLAR) | (1<<MUX3) | (1<<MUX2) | (1<<MUX1) | (0<<MUX0);
+#endif
+    delay(2);  // Let mux settle a little to get a more stable A/D conversion
+    // Start a conversion
+    ADCSRA |= _BV( ADSC );
+    // Wait for it to complete
+    while( ( (ADCSRA & (1<<ADSC)) != 0 ) );
+    // Scale the value
+    int results = (((InternalReferenceVoltage * 1023L) / ADC) + 5L) / 10L; // calculates for straight line value
+    return results;
 }
